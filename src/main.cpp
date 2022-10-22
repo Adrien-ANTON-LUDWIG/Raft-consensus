942b1faffa8251a0793e0e48418822bac8a093de
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-10-22 23:52:13 +0200

    [ADD] Raft: Leader election working (still dirty)

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <chrono>
 #include <iostream>
+#include <thread>
 
 enum STATE { FOLLOWER, CANDIDATE, LEADER };
 
@@ -19,13 +20,13 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
   /////////////////////////////////////////////////////////////////
-
+  std::srand(my_rank);
   bool worker_alive = true;
   int term = 0;
   int vote_count = 0;
   int vote_granted = false;
   int voted_for = -1;
-  auto election_timeout = std::chrono::milliseconds(std::rand() % 15000 + 150);
+  auto election_timeout = std::chrono::milliseconds(std::rand() % 150 + 150);
   if (my_rank == 0) election_timeout = std::chrono::milliseconds(100);
   auto heartbeat_timeout = std::chrono::milliseconds(50);
   auto start_time = std::chrono::system_clock::now();
@@ -34,28 +35,31 @@ int main(int argc, char **argv) {
   int message = -1;
   MPI_Status status;
   MPI_Request request = MPI_REQUEST_NULL;
-  bool requested = false;
 
   STATE state = FOLLOWER;
 
   // Print out information about MPI_COMM_WORLD
-  std::cout << "World Size: " << world_size << "   Rank: " << my_rank
-            << "  Election timeout: " << election_timeout.count() << std::endl;
+  std::cout << my_rank << ": Election timeout: " << election_timeout.count()
+            << std::endl;
 
   while (worker_alive) {
     current_time = std::chrono::system_clock::now();
 
     if (state == LEADER) {
       if (current_time - start_time >= heartbeat_timeout) {
-        // MPI_Send("Je suis encore en vie.");
-        // MPI_Bcast(&voted_for, 1, MPI_INT, my_rank, MPI_COMM_WORLD);
-        printf("Heartbeat send\n");
         start_time = std::chrono::system_clock::now();
-      }
-    }
 
-    else if (state == FOLLOWER) {
+        // std::cout << my_rank << ": Heartbeat send" << std::endl;
+
+        for (int rank = 0; rank < world_size; rank++) {
+          if (rank == my_rank) continue;
+          MPI_Send(&voted_for, 1, MPI_INT, rank, 51, MPI_COMM_WORLD);
+        }
+      }
+    } else if (state == FOLLOWER) {
       if (current_time - start_time >= election_timeout) {
+        if (request != MPI_REQUEST_NULL) MPI_Cancel(&request);
+
         state = CANDIDATE;
         term += 1;
         vote_count += 1;
@@ -63,46 +67,58 @@ int main(int argc, char **argv) {
         vote_granted = true;
 
         // Send Request vote messages
+        std::cout << my_rank << ": Send vote request" << std::endl;
+        for (int rank = 0; rank < world_size; rank++) {
+          if (rank == my_rank) continue;
+          MPI_Send(&voted_for, 1, MPI_INT, rank, 42, MPI_COMM_WORLD);
+        }
 
-        std::cout << "Send vote request from " << my_rank << std::endl;
-        MPI_Bcast(&voted_for, 1, MPI_INT, my_rank, MPI_COMM_WORLD);
       } else {
-        if (!requested && my_rank != 0) {
-          printf("Listen for message\n");
-          fflush(stdout);
+        if (request == MPI_REQUEST_NULL) {
+          // std::cout << my_rank << ": Listen for message" << std::endl;
           MPI_Irecv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
                     MPI_COMM_WORLD, &request);
-          requested = true;
         } else {
-          int flag = 0;
-          MPI_Test(&request, &flag, &status);
+          int request_completed = 0;
+          MPI_Test(&request, &request_completed, &status);
 
-          if (flag) {
-            std::cout << "Message received, responding..." << std::endl;
+          if (request_completed) {
+            // Reset timeout
+            start_time = std::chrono::system_clock::now();
 
-            if (!vote_granted) {
-              vote_granted = true;
-              voted_for = message;
+            if (status.MPI_TAG == 42) {
+              std::cout << my_rank << ": Received vote request from "
+                        << status.MPI_SOURCE << " responding..." << std::endl;
+
+              if (!vote_granted) {
+                vote_granted = true;
+                voted_for = message;
+              }
+
+              MPI_Send(&voted_for, 1, MPI_INT, status.MPI_SOURCE,
+                       status.MPI_TAG, MPI_COMM_WORLD);
             }
-            // MPI_Send(&voted_for, 1, MPI_INT, status.MPI_SOURCE, status.MPI_TAG,
-                    //  MPI_COMM_WORLD);
           }
         }
       }
     }
 
     else if (state == CANDIDATE) {
-      //   while (election_timeout > 0) {
-      //     MPI_Recv(..., timeout) = election_timeout);
-      //     // Update vote_count if needed
-      //   }
-      //   if (vote_count > world_size / 2) state = LEADER;
-      //   // Send something
-      //   else
-      //     state = FOLLOWER;
+      while (vote_count <= world_size / 2) {
+        MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                 MPI_COMM_WORLD, &status);
 
-      //   election_timeout = std::chrono::milliseconds(std::rand() % 150 +
-      //   150); start_time = std::chrono::system_clock::now();
+        if (message == my_rank) vote_count++;
+      }
+      if (vote_count > world_size / 2) {
+        state = LEADER;
+        std::cout << my_rank << ": is the new Leader" << std::endl;
+        // Send something
+      } else
+        state = FOLLOWER;
+
+      election_timeout = std::chrono::milliseconds(std::rand() % 150 + 150);
+      start_time = std::chrono::system_clock::now();
     }
   }
 
