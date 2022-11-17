a25585addddd7d12eb1fbb5f95b3ac4f4804c8c4
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-11-17 22:56:55 +0100

    [ADD] Client: attributes and parseCommand()

--- src/client.cpp
+++ src/client.cpp
@@ -1,6 +1,8 @@
 #include "client.hh"
 
 #include <fstream>
+#include <sstream>
+#include <string>
 
 #include "messages/CMD/load.hh"
 #include "messages/mpi_wrappers.hh"
@@ -10,6 +12,9 @@ using namespace MessageNS;
 Client::Client(int id, int nbServer) : m_id(id), m_nbServer(nbServer) {}
 
 void Client::update() {
+  // Update time
+  m_currentTime = std::chrono::system_clock::now();
+
   // Create message
   CMD::Load message("test.txt", m_id);
 
@@ -22,9 +27,9 @@ void Client::update() {
   if (!status.has_value()) return;
 
   // if (!response.success) {
-    // Look leaderId
-    // Save it
-    // Send message to the leader
+  // Look leaderId
+  // Save it
+  // Send message to the leader
   // }
 }
 
@@ -36,21 +41,73 @@ void Client::loadCommands(const std::string& path) {
     while (std::getline(stream, line)) {
       if (line[0] == '-') {
         client_rank++;
-      }
-      else if (client_rank == m_id)
-      {
+      } else if (client_rank == m_id) {
         if (line[0] == '$') {
           // TODO specials instructions
           std::string cmd = line.substr(line.find(' '));
-        }
-        else if (line[0] != '#') {
+        } else if (line[0] != '#') {
           size_t commentStart = line.find('#');
           if (commentStart != std::string::npos)
             line = line.substr(0, commentStart);
 
-          m_commands.push_back(line);
+          Message command = parseCommand(line);
+          m_commands.push_back(command);
         }
       }
     }
+  }
+}
+
+Message Client::parseCommand(const std::string& command) {
+  std::istringstream iss(command);
+  std::vector<std::string> tokens;
+  std::string t;
+
+  while (iss >> t && tokens.size() < 3) tokens.push_back(t);
+  getline(iss, t);
+  tokens.push_back(t);
+
+  auto& cmd = tokens[0];
+  cmd == "LOAD";
+
+  if (cmd == "LOAD") {
+    if (tokens.size() < 2)
+      throw std::invalid_argument(
+          "Missing filename. Usage: LOAD <file_name>\n");
+    else if (tokens.size() > 2)
+      throw std::invalid_argument(
+          "Too many arguments. Usage: LOAD <file_name>\n");
+
+    std::string file_name = tokens[1];
+    return CMD::Load(file_name, m_id);
+  } else if (cmd == "LIST") {
+    if (tokens.size() != 1)
+      throw std::invalid_argument("Too many arguments. Usage: LIST\n");
+
+    // return CMD::List(m_id);
+    throw std::logic_error("Not implemented yet\n");
+  } else if (cmd == "DELETE") {
+    if (tokens.size() < 2)
+      throw std::invalid_argument(
+          "Missing file index. Usage: DELETE <file_index>\n");
+    else if (tokens.size() > 2)
+      throw std::invalid_argument(
+          "Too many arguments. Usage: DELETE <file_index>\n");
+
+    int file_index = std::stoi(tokens[1]);
+    // return CMD::Delete(file_index, m_id);
+    throw std::logic_error("Not implemented yet\n");
+  } else if (cmd == "APPEND") {
+    if (tokens.size() < 3)
+      throw std::invalid_argument(
+          "Missing file index and/or data. Usage: APPEND <file_index> "
+          "<data>\n");
+
+    int file_index = std::stoi(tokens[1]);
+    std::string data = tokens[2];
+    // return CMD::Append(file_index, data, m_id);
+    throw std::logic_error("Not implemented yet\n");
+  } else {
+    throw std::invalid_argument("Unknown command");
   }
 }
--- src/client.hh
+++ src/client.hh
@@ -1,29 +1,47 @@
 #pragma once
 
+#include <chrono>
 #include <string>
 #include <vector>
 
+#include "messages/message.hh"
 #include "repl_process.hh"
+#include "uuid.h"
 
 class Client : REPL::Process {
-private:
+ private:
+  /**
+   * State
+   */
   int m_id;
   int m_nbServer;
   int m_leaderId = 0;
 
-  std::vector<std::string> m_commands;
+  /**
+   * Commands management
+   */
+  std::vector<MessageNS::Message> m_commands;
+  int m_currentCommand = 0;
+  std::vector<uuids::uuid> m_filesUUID;
+
+  /**
+   * Time management
+   */
+  std::chrono::milliseconds m_requestTimeout;
+  std::chrono::_V2::system_clock::time_point m_startTime;
+  std::chrono::_V2::system_clock::time_point m_currentTime;
 
   // REPL
   bool m_isCrashed;
   bool m_isStarted;
-  
 
-public:
+ public:
   Client(int id, int nbServer);
 
   void update();
 
   void loadCommands(const std::string& path);
+  MessageNS::Message parseCommand(const std::string& command);
 
   // REPL HANDLERS
   virtual void handleREPLInfo(const json& json) override;
