e2646b367a758fa91bbd4824a104cd6fa69a28c4
Adrien ANTON LUDWIG <adrien.antonludwig@gmail.com>
2022-11-17 16:04:42 +0100

    [ADD] AppendEntriesResponse: match index attributes

--- src/messages/RPC/appendEntriesResponse.cpp
+++ src/messages/RPC/appendEntriesResponse.cpp
@@ -6,14 +6,27 @@ AppendEntriesResponse::AppendEntriesResponse(int term, bool success,
     : Message(Type::RPC_APPEND_ENTRIES_RESPONSE, originId),
       m_success(success) {}
 
+AppendEntriesResponse::AppendEntriesResponse(const json& data) : Message(data) {
+  m_term = data["term"];
+  m_success = data["success"];
+  m_matchIndex = data["matchIndex"];
+}
+
 bool AppendEntriesResponse::isSuccess() const { return m_success; }
 int AppendEntriesResponse::getTerm() const { return m_term; }
+int AppendEntriesResponse::getMatchIndex() const { return m_matchIndex; }
+
+void AppendEntriesResponse::setSuccess(bool success) { m_success = success; }
+void AppendEntriesResponse::setMatchIndex(int matchIndex) {
+  m_matchIndex = matchIndex;
+}
 
 json AppendEntriesResponse::toJSON() const {
   json data = Message::toJSON();
 
   data["term"] = m_term;
   data["success"] = m_success;
+  data["matchIndex"] = m_matchIndex;
 
   return data;
 }
--- src/messages/RPC/appendEntriesResponse.hh
+++ src/messages/RPC/appendEntriesResponse.hh
@@ -7,12 +7,18 @@ class AppendEntriesResponse : public Message {
  private:
   int m_term;
   bool m_success;
+  int m_matchIndex = -1;
 
  public:
   AppendEntriesResponse(int term, bool success, int originId);
+  AppendEntriesResponse(const json& data);
 
   bool isSuccess() const;
   int getTerm() const;
+  int getMatchIndex() const;
+
+  void setSuccess(bool success);
+  void setMatchIndex(int matchIndex);
 
   virtual json toJSON() const override;
 };
